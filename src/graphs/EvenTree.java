dd18d1434f02b2ac824dc5e85b9f9a0bdf8f8c33
Tyrone Hinderson <thinderson@conductor.com>
2014-08-17 14:17:13 -0400

    solved Even Tree

--- src/graphs/EvenTree.java
+++ src/graphs/EvenTree.java
@@ -1,19 +1,45 @@
 package graphs;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * <p>
- * Solution to the "even tree" problem
- * Check out <a href="https://www.hackerrank.com/challenges/even-tree">the challenge</a>
+ * Solution to the "even tree" problem Check out <a href="https://www.hackerrank.com/challenges/even-tree">the
+ * challenge</a>
+ * </p>
+ * <p>
+ * note: you'll need to rename the class to "Solution" and remove the package declaration above before running on
+ * Hackerrank.com
  * </p>
  * 2014
- *
+ * 
  * @author Tyrone Hinderson
  */
 public class EvenTree {
-    private static class Vertex{
-        private HashMap<Integer, Integer> neighborCounts;
-        private int id;
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        String[] counts = stdin.readLine().split(" ");
+        int nodes = Integer.parseInt(counts[0]);
+        int[] childToParent = new int[nodes + 1];
+        int[] nodeTotals = new int[nodes + 1];
+        for (int i = 0; i < nodeTotals.length; i++) {
+            nodeTotals[i] = 1;
+        }
+
+        for (int i = 0; i < Integer.parseInt(counts[1]); i++) {
+            String[] edge = stdin.readLine().split(" ");
+            childToParent[Integer.parseInt(edge[0])] = Integer.parseInt(edge[1]);
+        }
+
+        int edgesToCut = 0;
+        for (int i = nodes; i > 1; i--) {
+            int nodeTotal = nodeTotals[i];
+            if (nodeTotal % 2 == 0)
+                edgesToCut++;
+            nodeTotals[childToParent[i]] += nodeTotal;
+        }
+        System.out.println(edgesToCut);
     }
 }
