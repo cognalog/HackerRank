1d0cf135c9f4d8a9b4012e5b17ce0c3817f4c739
Tyrone Hinderson <thinderson@conductor.com>
2014-08-16 21:54:03 -0400

    moved everything around in Intellij

---
+++ src/graphs/EvenTree.java
@@ -0,0 +1,19 @@
+package graphs;
+
+import java.util.HashMap;
+
+/**
+ * <p>
+ * Solution to the "even tree" problem
+ * Check out <a href="https://www.hackerrank.com/challenges/even-tree">the challenge</a>
+ * </p>
+ * 2014
+ *
+ * @author Tyrone Hinderson
+ */
+public class EvenTree {
+    private static class Vertex{
+        private HashMap<Integer, Integer> neighborCounts;
+        private int id;
+    }
+}
